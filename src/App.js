f04a651bd03c983f01a6b3a04dc39a256bd29984
Louise van den Berg <louisevdb84@gmail.com>
2018-05-09 14:27:16 +0200

    #3 Part 2 of Tutorial plus unstyled calender functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
+import Calender from './Calender';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
+
+  getDayName(number) {    
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];    
+    return days[number];
+  }
+
+  render() {        
+    let today = new Date().getDay();
+    if(today === 6 || today === 0)
+      return (
+        <div>
+          <div>Today is {this.getDayName(today)} - You can watch TV all day today</div>
+          <Calender></Calender>
+        </div>);
+    else
+      return (
+        <div>
+          <div>Today is {this.getDayName(today)} - You should go to work today</div>
+          <Calender></Calender>
+        </div>);
+  
+    
   }
 }
 
