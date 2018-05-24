80c6ab3fa330a724cf1c0d0cebbab132f4d5d6fd
Louise van den Berg <louisevdb84@gmail.com>
2018-05-24 14:16:21 +0200

    #6 Tutorial Part 5 completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import MonthChecker from './Test/Part3/MonthChecker';
 import Header from './Header';
 import Calender from './Test/Part2/Calender';
 import CalenderV2 from './Test/Part3/Calendar-v2';
+import MealPlan from './MealPlan';
 
 
 class App extends Component {
   
   render() {
     return [
+      <MealPlan/>,
       <Header />,      
       <DayChecker />,         
       <MonthChecker />,      
