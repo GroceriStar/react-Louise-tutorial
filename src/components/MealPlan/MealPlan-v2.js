016884d082729cae65ebfe88a3a741fac29ba392
Louise van den Berg <louisevdb84@gmail.com>
2018-06-05 14:54:10 +0200

    #18 Add from with form submit handler

--- src/components/MealPlan/MealPlan-v2.js
+++ src/components/MealPlan/MealPlan-v2.js
@@ -4,12 +4,12 @@ import _ from 'lodash';
 class MealPlan extends Component {
     constructor(props) {
         super(props);
-        this.state = {           
+        this.state = {            
             meals: []
           };
     }
  
-    renderMeals() {
+    renderMeals() {        
         return _.map(this.state.meals, meal =>
             <div>
                 <li>{meal.name}</li>    
@@ -19,37 +19,43 @@ class MealPlan extends Component {
                 <br />
             </div>);
     }
-  
-    onAddMeal = () => {        
-        var meal = {};
 
-        meal.name = document.getElementById("name").value;        
-        meal.description = document.getElementById("description").value;        
-        meal.steps = document.getElementById("steps").value;        
-        meal.imageUrl = document.getElementById("imageUrl").value;        
+    addMeal = (event) => {
+        //so that the whole page doesnt reload 
+        event.preventDefault();     
+        
+        const data = new FormData(event.target);
+
+        var meal = {};
+        meal.name = data.get('name');
+        meal.description = data.get('description');
+        meal.steps = data.get('steps');
+        meal.imageUrl = data.get('imageUrl');
         
         this.setState({ meals: [...this.state.meals, meal] })
-}
+    }
 
-render() {
-    return(
-      <div>
-            <h2>Today you should eat this</h2>
-            <label>Name</label><br/>
-            <input id="name" type="text" /><br/>    
-            <label>Description</label><br/>
-            <textarea id="description" type="text" /><br />
-            <label>Steps</label><br/>
-            <textarea id="steps" type="text" /><br />
-            <label>Image URL</label><br/>
-            <input id="imageUrl" type="text" /><br/>
-        <br/>    
-        <button onClick={this.onAddMeal}>Add meal</button> <br/>
-        <ul>
-          {this.renderMeals()}
-        </ul>
-      </div>
-    );
-  }
+    render() {        
+        return (        
+        <div>
+                <h2>Today you should eat this</h2>
+                <form onSubmit={this.addMeal}>
+                    <label>Name</label><br/>
+                    <input name="name" type="text" /><br/>    
+                    <label>Description</label><br/>
+                    <textarea name="description" type="text" /><br />
+                    <label>Steps</label><br/>
+                    <textarea name="steps" type="text" /><br />
+                    <label>Image URL</label><br/>
+                    <input name="imageUrl" type="text" /><br />                                
+                    <br />
+                    <input type="submit" value="Add meal" />
+                </form>        
+            <ul>
+            {this.renderMeals()}
+            </ul>
+        </div>
+        );
+    }
 }
 export default MealPlan;
