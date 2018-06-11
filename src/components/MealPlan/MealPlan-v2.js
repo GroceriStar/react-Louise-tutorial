95be1a2ad9ae396e36655493d0a9d34ae27f3a3c
Louise van den Berg <louisevdb84@gmail.com>
2018-06-11 12:30:02 +0200

    #9 completed test of tutorial part 7

--- src/components/MealPlan/MealPlan-v2.js
+++ src/components/MealPlan/MealPlan-v2.js
@@ -14,7 +14,8 @@ class MealPlan extends Component {
             <div>
                 <li>{meal.name}</li>    
                 <li>{meal.description}</li>
-                <li>{meal.steps}</li>
+                <li>{meal.ingredients}</li>
+                <li>{meal.directions}</li>
                 <img src={meal.imageUrl} width="200px" alt="Meal"></img>
                 <br />
             </div>);
@@ -29,10 +30,40 @@ class MealPlan extends Component {
         var meal = {};
         meal.name = data.get('name');
         meal.description = data.get('description');
-        meal.steps = data.get('steps');
+        meal.ingredients = data.get('ingredients');
+        meal.directions = data.get('directions');
         meal.imageUrl = data.get('imageUrl');
         
         this.setState({ meals: [...this.state.meals, meal] })
+    }
+
+    componentWillMount() {        
+        var meal = [
+            {
+                name : "Pancakes",
+                description : "Delicious fruit pancakes",
+                directions : ["1. Mix dry ingredients ", "2. Add milk and eggs ", "3. Pour into pan "],
+                ingredients : ["Flour ", "Eggs ", "Milk "],
+                imageUrl : "https://images.unsplash.com/photo-1464219551459-ac14ae01fbe0?ixlib=rb-0.3.5&s=ca7cadf4438b09eafc98ef45a2d0546c&auto=format&fit=crop&w=1050&q=80"
+            },
+            {
+                name : "Flapjacks",
+                description : "Delicious flapjacks recipe",
+                directions : ["1. Mix dry ingredients ", "2. Add milk and eggs ", "3. Pour into pan "],
+                ingredients : ["Flour ", "Eggs ", "Milk ", "Sugar ", "Baking powder"],
+                imageUrl : "https://images.unsplash.com/photo-1518449065149-843e338dc070?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=dfd1350f43ffe56f19fa6278d64dc893&auto=format&fit=crop&w=634&q=80"
+            },
+            {
+                name : "Scrambled Eggs",
+                description : "Basic recipe",
+                directions : ["1. Beat eggs ", "2. Heat butter ", "3. Pour into pan "],
+                ingredients : ["Butter ", "Eggs ", "Milk "],
+                imageUrl : "https://images.unsplash.com/photo-1524250981170-bd522ef63fbf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9f18b175dbfd17d1511d924ad03aab2f&auto=format&fit=crop&w=634&q=80"
+            }
+        ]
+        
+        this.setState({ meals: meal });
+        
     }
 
     render() {        
@@ -44,8 +75,10 @@ class MealPlan extends Component {
                     <input name="name" type="text" /><br/>    
                     <label>Description</label><br/>
                     <textarea name="description" type="text" /><br />
-                    <label>Steps</label><br/>
-                    <textarea name="steps" type="text" /><br />
+                    <label>Ingredients</label><br/>
+                    <textarea name="ingredients" type="text" /><br />
+                    <label>Directions</label><br/>
+                    <textarea name="directions" type="text" /><br />
                     <label>Image URL</label><br/>
                     <input name="imageUrl" type="text" /><br />                                
                     <br />
